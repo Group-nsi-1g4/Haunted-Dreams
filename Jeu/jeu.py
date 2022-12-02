d025ca5e1aaed2a416c82312e3d6456e7bbf9e7e
math0br <mathieu0brodard@gmail.com>
2022-12-02 23:28:23 +0100

    Different ajout au code
    -position du curseur de la souris en haut a gauche (curseur marque en rose)
    -Agrandisement de la map
    -Debut de flashlight
    -Supression de la hitbox rose
    -Le joueur ne pe pas sortir de la map

--- Jeu/jeu.py
+++ Jeu/jeu.py
@@ -11,36 +11,31 @@ global x2
 global y2
 global x3
 global y3
+global x4
+global y4
 x1,y1=0,57
 x2,y2=50,57
-x3=60
-y3=57
-Player1 = {'position':[x1,y1] , 'Vitesse': 2,}
-bot2 = {'position':[x2,y2] , 'Vitesse': 0.09}
-bot1= {'position':[x3,y3] , 'Vitesse': 0.05}
+x3,y3=60,57
+x4,y4=100,100
+Player1 = {'position':[x1,y1] , 'Vitesse': 3,}
+bot2 = {'position':[x2,y2] , 'Vitesse': 0.04}
+bot1= {'position':[x3,y3] , 'Vitesse': 0.02}
 
-test
 
+    
 def deplacement1():
     global x1,y1
-    colorD=pyxel.pget(x1+8,y1+15)
-    colorU=pyxel.pget(x1+8,y1)
-    colorR=pyxel.pget(x1+13,y1+7)
-    colorL=pyxel.pget(x1+2,y1+7)
-    if pyxel.btn(pyxel.KEY_P):
-        Player1['Vitesse']=Player1['Vitesse']+1
-    if pyxel.btn(pyxel.KEY_M):
-        Player1['Vitesse']=Player1['Vitesse']-1
-    if colorD !=14:
+    
+    if y1+15<159:
         if pyxel.btn(pyxel.KEY_DOWN):
             y1=y1+Player1['Vitesse']
-    if colorR !=14:   
+    if x1+13<319:   
         if pyxel.btn(pyxel.KEY_RIGHT):
             x1=x1+Player1['Vitesse']
-    if colorL !=14 :
+    if x1+1>0 :
         if pyxel.btn(pyxel.KEY_LEFT):
             x1=x1-Player1['Vitesse']
-    if colorU !=14 :
+    if y1>0 :
         if pyxel.btn(pyxel.KEY_UP):
             y1=y1-Player1['Vitesse']
 def ai2():
@@ -55,12 +50,18 @@ def ai1():
 
 def flash():
     global x1,y1
+    pyxel.circ(x1+8,y1+8,20,1)
     pyxel.circb(x1+8,y1+8,20,1)
     pyxel.circb(x1+8,y1+8,19,1)
     pyxel.circb(x1+8,y1+8,21,5)
+    
+def curseur():
+    global x4,y4
+    x4=pyxel.mouse_x
+    y4=pyxel.mouse_y
 class App:
     def __init__(self):
-        pyxel.init(160, 80, title="Projet jeu Pyxel")
+        pyxel.init(320,160 , title="Projet jeu Pyxel")
         pyxel.load("media.pyxres")
         pyxel.run(self.update, self.draw)
 
@@ -70,13 +71,19 @@ class App:
         deplacement1() 
         ai2()
         ai1()
+        curseur()
 
     def draw(self):
         pyxel.cls(0)
         pyxel.blt(0,0,0,0,0,320,320,0)
+        flash()
         pyxel.blt(x1,y1,1,0,0,16,16,0)
         pyxel.blt(x2,y2,1,16,0,16,16,0)
         pyxel.blt(x3,y3,1,32,0,16,16,0)
-        flash()
+        pyxel.text(0,0,str(x4),14)
+        pyxel.text(15,0,str(y4),14)
+        pyxel.pset(x4,y4,14)
+        
+        
 
 App()
