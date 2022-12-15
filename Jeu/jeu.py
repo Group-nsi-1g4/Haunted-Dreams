e3f4819a397d53cbfbc3a83a2dacb1cc630b0e14
AztherOmpus <119583355+AztherOmpus@users.noreply.github.com>
2022-12-15 11:31:42 +0100

    Add files via upload
    -Flash light amélioré avec Vue modifiable
    -Stamina avec course en appuiant sur Shift
    -Hashtag devant chaque fonctions pour plus de compréhension

--- Jeu/jeu.py
+++ Jeu/jeu.py
@@ -1,7 +1,9 @@
 #Mathieu brodard, Mathieu vilmot, Mohamed Latreche, Sailan Sivakumar
 import pyxel
+import time
 
-
+#Ajout: Flash fonctionnel sur les ennemis uniquement
+#       Stamina et course (Shift pour courir)
 
 global Player1
 global Player2
@@ -17,15 +19,17 @@ x1,y1=0,57
 x2,y2=50,57
 x3,y3=60,57
 x4,y4=100,100
-Player1 = {'position':[x1,y1] , 'Vitesse': 3,}
+#Dictionnaires des ennnemis et joueurs
+Player1 = {'position':[x1,y1] , 'Vitesse': 2, 'Stamina': 100, 'Vue' : 25}
 bot2 = {'position':[x2,y2] , 'Vitesse': 0.04}
 bot1= {'position':[x3,y3] , 'Vitesse': 0.02}
 
 
+
     
 def deplacement1():
-    global x1,y1
-    
+    global y1,x1
+#Déplacement haut,bas,gauche,droite
     if y1+15<159:
         if pyxel.btn(pyxel.KEY_DOWN):
             y1=y1+Player1['Vitesse']
@@ -38,27 +42,48 @@ def deplacement1():
     if y1>0 :
         if pyxel.btn(pyxel.KEY_UP):
             y1=y1-Player1['Vitesse']
+            
+#Ennemis qui suit le joueur
 def ai2():
-    global x2,y2
+    global x2,y2,Vue,x1,y1
     x2=x2+((x1-x2)*bot2['Vitesse'])
     y2=y2+((y1-y2)*bot2['Vitesse'])
-
 def ai1():
     global x3,y3
     x3=x3+((x1-x3)*bot1['Vitesse'])
     y3=y3+((y1-y3)*bot1['Vitesse'])
 
+#Lumière autour du joueur
 def flash():
     global x1,y1
-    pyxel.circ(x1+8,y1+8,20,1)
-    pyxel.circb(x1+8,y1+8,20,1)
-    pyxel.circb(x1+8,y1+8,19,1)
-    pyxel.circb(x1+8,y1+8,21,5)
-    
+    pyxel.circ(x1+8,y1+8,Player1['Vue'],5)
+    pyxel.circ(x1+8,y1+8,Player1['Vue']-8,12)
+    pyxel.circ(x1+8,y1+8,Player1['Vue']-16,6)
+
+#Position du curseur 
 def curseur():
     global x4,y4
     x4=pyxel.mouse_x
     y4=pyxel.mouse_y
+    
+#Course et système de Stamina
+def Course():
+    global Player1 
+    if pyxel.btn(pyxel.KEY_SHIFT) and Player1['Stamina'] >= 0:
+        Player1['Vitesse']=4
+        Player1['Stamina']= Player1['Stamina']-2
+    else:
+#Si le joueur a peu de Stamina : sa vitesse est réduite temporairement
+        if Player1['Stamina']<=25:
+            Player1['Vitesse']=1
+        else:
+            Player1['Vitesse']=2
+        Player1['Stamina']= Player1['Stamina']+0.5
+#Stamina ne dépasse pas 100%
+    if  Player1['Stamina'] >= 101 :
+        Player1['Stamina']= Player1['Stamina']-1
+
+
 class App:
     def __init__(self):
         pyxel.init(320,160 , title="Projet jeu Pyxel")
@@ -72,16 +97,27 @@ class App:
         ai2()
         ai1()
         curseur()
+        Course()
 
     def draw(self):
         pyxel.cls(0)
         pyxel.blt(0,0,0,0,0,320,320,0)
         flash()
         pyxel.blt(x1,y1,1,0,0,16,16,0)
-        pyxel.blt(x2,y2,1,16,0,16,16,0)
-        pyxel.blt(x3,y3,1,32,0,16,16,0)
+#Si l'ennemis est dans le flash : il est visible
+        if ((-x2+x1) < Player1['Vue'] and (x2-x1) < Player1['Vue']) and ((-y2+y1) < Player1['Vue'] and (y2-y1) < Player1['Vue'])  :
+            pyxel.blt(x2,y2,1,16,0,16,16,0)
+        if ((-x3+x1) < Player1['Vue'] and (x3-x1) < Player1['Vue']) and ((-y3+y1) < Player1['Vue'] and (y3-y1) < Player1['Vue'])  :
+            pyxel.blt(x3,y3,1,32,0,16,16,0)
+#Position du curseur
         pyxel.text(0,0,str(x4),14)
         pyxel.text(15,0,str(y4),14)
+#Stamina en haut a gauche (si elle est faible : elle devient rouge)
+        if Player1['Stamina'] >= 25 :
+            pyxel.text(0,10,str(Player1['Stamina']),6)
+        else:
+            pyxel.text(0,10,str(Player1['Stamina']),8)
+#Curseur Visible
         pyxel.pset(x4,y4,14)
         
         
---
+++ README.txt
@@ -0,0 +1,5 @@
+Petit update sur ce que Mathieu a fait :
+Vu qu'on a pas encore mis les points clairement sur les i mais qu'on a une vague idée du jeu, 
+j'ai repris le code n°1 et l'a modifier pour créer le flash "fonctionnel" (uniquement les ennemis) et le fait de courir
+
+J'ai aussi mis des annotations au dessus des différentes fonctions pour plus de compréhension.
