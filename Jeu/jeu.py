5bcfd8f4f6bff3f3d588b9dbcadce7126473ebb9
math0br <119584912+math0br@users.noreply.github.com>
2022-12-15 23:34:47 +0100

    remise en place des fichier
    remis le code principal ici
    ajouter un fichier .bat (cmd) qui te permet de direct acceder a l'édit pour les graphisme (sans avoir besoin d'ouvrir un fichier tt ça) plus pratique quand on est sous windows
    (corriger le code principale qui buggai de ouf jsp pk j'ai du toucher a un truc sans m'en rendre compte lmao)

--- Jeu/jeu.py
+++ Jeu/jeu.py
@@ -7,9 +7,6 @@ import time
             Quelque changement dans media.pyxres : page 2 pour les menus et page 1 avec les différent sens des personnages
 '''
 global start
-#Ajout: Flash fonctionnel sur les ennemis uniquement
-#       Stamina et course (Shift pour courir)
-
 global Player1
 global Player2
 global x1
@@ -25,23 +22,17 @@ x2,y2=50,57
 x3,y3=60,57
 x4,y4=100,100
 #Dictionnaires des ennnemis et joueurs
-Player1 = {'position':[x1,y1] , 'Vitesse': 2, 'Stamina': 100, 'Vue' : 50, 'Sens' : 'Droite'}
+Player1 = {'position':[x1,y1] , 'Vitesse': 2, 'Stamina': 100, 'Vue' : 25, 'Sens' : 'Droite'}
 bot2 = {'position':[x2,y2] , 'Vitesse': 0.04 , 'Sens' : 'Droite'}
 bot1= {'position':[x3,y3] , 'Vitesse': 0.02 , 'Sens' : 'Droite'}
 start=0
 
-Player1 = {'position':[x1,y1] , 'Vitesse': 2, 'Stamina': 100, 'Vue' : 25}
-bot2 = {'position':[x2,y2] , 'Vitesse': 0.04}
-bot1= {'position':[x3,y3] , 'Vitesse': 0.02}
 
 
 
-    
 def deplacement1():
     global y1,x1,Player1
 #Déplacement haut,bas,gauche,droite avec detections de sens droite/gauche
-    global y1,x1
-#Déplacement haut,bas,gauche,droite
     if y1+15<159:
         if pyxel.btn(pyxel.KEY_DOWN):
             y1=y1+Player1['Vitesse']
@@ -56,7 +47,7 @@ def deplacement1():
     if y1>0 :
         if pyxel.btn(pyxel.KEY_UP):
             y1=y1-Player1['Vitesse']
-            
+
 #Ennemis qui suit le joueur avec detection de sens
 def ai2():
     global x2,y2,x1,y1
@@ -66,12 +57,6 @@ def ai2():
         bot2['Sens']='Droite'
     if x1-x2 <= 0:
         bot2['Sens']='Gauche'
-#Ennemis qui suit le joueur
-def ai2():
-    global x2,y2,Vue,x1,y1
-    x2=x2+((x1-x2)*bot2['Vitesse'])
-    y2=y2+((y1-y2)*bot2['Vitesse'])
-
 def ai1():
     global x3,y3
     x3=x3+((x1-x3)*bot1['Vitesse'])
@@ -82,8 +67,6 @@ def ai1():
         bot1['Sens']='Gauche'
 
 #Lumière autour du joueur par rapport à la vue
-#Lumière autour du joueur
-
 def flash():
     global x1,y1
     pyxel.circ(x1+8,y1+8,Player1['Vue'],5)
@@ -95,7 +78,7 @@ def curseur():
     global x4,y4
     x4=pyxel.mouse_x
     y4=pyxel.mouse_y
-    
+
 #Course et système de Stamina
 def Course():
     global Player1 
@@ -115,7 +98,7 @@ def Course():
 #Stamina ne dépasse pas 100%
     if  Player1['Stamina'] >= 101 :
         Player1['Stamina']= Player1['Stamina']-1
-        
+
 #Le Menu principale dans sont intégralité : Jouer et Quitter pour l'instant
 def menu():
     global x4,y4,start,x1,x2,x3,y1,y2,y3,bot1,bot2,Player1
@@ -136,12 +119,6 @@ def menu():
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
             pyxel.quit()
 
-    
-        Player1['Stamina']= Player1['Stamina']+0.5
-#Stamina ne dépasse pas 100%
-    if  Player1['Stamina'] >= 101 :
-        Player1['Stamina']= Player1['Stamina']-1
-
 
 class App:
     def __init__(self):
@@ -155,7 +132,7 @@ class App:
 
     def update(self):
         global start
-        
+
 #Appuier sur TAB pour ouvrir le menu ET stopper le jeu
         if pyxel.btnp(pyxel.KEY_TAB):
             start=0
@@ -166,7 +143,7 @@ class App:
             ai1()
             curseur()
             Course()
-            
+
 #Si la partie n'est pas démarrée
         elif start==0:
             menu()
@@ -176,7 +153,7 @@ class App:
 
     def draw(self):
         global start
-        
+
 #Si la partie est démarrée
         if start==1:
             pyxel.cls(0)
@@ -186,7 +163,7 @@ class App:
                 pyxel.blt(x1,y1,1,0,0,16,16,0)
             elif Player1['Sens']=='Gauche':
                 pyxel.blt(x1,y1,1,0,16,16,16,0)
-                
+
 #Si l'ennemis est dans le flash : il est visible
 # + Si il regarde a droite : sprite a droite et inversement
             if ((-x2+x1) < Player1['Vue'] and (x2-x1) < Player1['Vue']) and ((-y2+y1) < Player1['Vue'] and (y2-y1) < Player1['Vue'])  :
@@ -194,59 +171,30 @@ class App:
                     pyxel.blt(x2,y2,1,16,0,16,16,0)
                 elif bot2['Sens']=='Gauche':
                     pyxel.blt(x2,y2,1,16,16,16,16,0)
-                    
+
 #Si le joueur regarde a droite : sprite a droite et inversement
             if ((-x3+x1) < Player1['Vue'] and (x3-x1) < Player1['Vue']) and ((-y3+y1) < Player1['Vue'] and (y3-y1) < Player1['Vue'])  :
                 if bot1['Sens']=='Droite':
                     pyxel.blt(x3,y3,1,32,0,16,16,0)
                 if bot1['Sens']=='Gauche':
                     pyxel.blt(x3,y3,1,32,16,16,16,0)
-                    
+
 #Position du curseur
             pyxel.text(0,0,str(x4),14)
             pyxel.text(15,0,str(y4),14)
-            
+
 #Stamina en haut a gauche (si elle est faible : elle devient rouge)
             if Player1['Stamina'] >= 25 :
                 pyxel.text(0,10,str(Player1['Stamina']),6)
             else:
                 pyxel.text(0,10,str(Player1['Stamina']),8)
-                
+
 #Curseur Visible
             pyxel.pset(x4,y4,14)
         if start==0:
             pyxel.blt(x4,y4,2,48,0,6,6,0)
-        
 
-        if pyxel.btnp(pyxel.KEY_ESCAPE):
-            pyxel.quit()
-        deplacement1() 
-        ai2()
-        ai1()
-        curseur()
-        Course()
 
-    def draw(self):
-        pyxel.cls(0)
-        pyxel.blt(0,0,0,0,0,320,320,0)
-        flash()
-        pyxel.blt(x1,y1,1,0,0,16,16,0)
-#Si l'ennemis est dans le flash : il est visible
-        if ((-x2+x1) < Player1['Vue'] and (x2-x1) < Player1['Vue']) and ((-y2+y1) < Player1['Vue'] and (y2-y1) < Player1['Vue'])  :
-            pyxel.blt(x2,y2,1,16,0,16,16,0)
-        if ((-x3+x1) < Player1['Vue'] and (x3-x1) < Player1['Vue']) and ((-y3+y1) < Player1['Vue'] and (y3-y1) < Player1['Vue'])  :
-            pyxel.blt(x3,y3,1,32,0,16,16,0)
-#Position du curseur
-        pyxel.text(0,0,str(x4),14)
-        pyxel.text(15,0,str(y4),14)
-#Stamina en haut a gauche (si elle est faible : elle devient rouge)
-        if Player1['Stamina'] >= 25 :
-            pyxel.text(0,10,str(Player1['Stamina']),6)
-        else:
-            pyxel.text(0,10,str(Player1['Stamina']),8)
-#Curseur Visible
-        pyxel.pset(x4,y4,14)
-        
-        
+
 
 App()
